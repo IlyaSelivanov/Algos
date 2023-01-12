f9aefe3dd3070a959f56afee5cb346310430cb05
Ilya <selivanov.ilya@gmail.com>
2023-01-12 16:10:57 +0200

    selection sort

--- src/Algos/Extensions/Sorting.cs
+++ src/Algos/Extensions/Sorting.cs
@@ -41,5 +41,49 @@ namespace Algos.Algorithmes
 
             return array;
         }
+
+        public static T[] SelectionSortAsc<T>(this T[] array)
+            where T : IComparable
+        {
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = i;
+                int j = i + 1;
+
+                while(j < array.Length)
+                {
+                    if(array[j].CompareTo(array[i]) < 0)
+                        minIndex = j;
+                }
+
+                var tmp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = tmp;
+            }
+
+            return array;
+        }
+
+        public static T[] SelectionSortDesc<T>(this T[] array)
+            where T : IComparable
+        {
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = i;
+                int j = i + 1;
+
+                while(j < array.Length)
+                {
+                    if(array[j].CompareTo(array[i]) > 0)
+                        minIndex = j;
+                }
+
+                var tmp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = tmp;
+            }
+
+            return array;
+        }
     }
 }
